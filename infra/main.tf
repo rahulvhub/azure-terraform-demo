2b1f358bdca6cf32a394450e482e7524285090e3
Rahul Verma <rahulverma1@hotmail.com>
2026-03-19 18:44:09 +0530

    deployapp

--- infra/main.tf
+++ infra/main.tf
@@ -1,1 +1,20 @@
+resource "azurerm_resource_group" "app" {
+  name     = "rg-webapp-prod"
+  location = "East US"
+}
 
+resource "azurerm_service_plan" "plan" {
+  name                = "asp-mywebapp"
+  resource_group_name = azurerm_resource_group.app.name
+  location            = azurerm_resource_group.app.location
+  os_type             = "Linux"
+  sku_name            = "F1" 
+}
+
+resource "azurerm_linux_web_app" "web" {
+  name                = "webapp-unique-name-2026" # CHANGE TO BE UNIQUE
+  resource_group_name = azurerm_resource_group.app.name
+  location            = azurerm_resource_group.app.location
+  service_plan_id     = azurerm_service_plan.plan.id
+  site_config {}
+}
